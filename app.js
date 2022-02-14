dc6c8697a672e08e86835db4a5b9dae067170cdc
h8 <h.latifi03@gmail.com>
2022-02-14 23:23:37 +0330

    bind

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-let hi="hi";
-console.log(hi);
+class Person{
+    constructor(name , ssn , age){
+        this.name=name ,
+        this.ssn=ssn ,
+        this.age=age
+    }
+}
+
+
+
+
+function printName(){
+    console.log(this.name)
+
+}
+let newPrintName=printName.bind(new Person("reza","55849799999",25));
+newPrintName();
