73b02787bf61b0ccd87612186b338bcfb36fa7aa
h8 <h.latifi03@gmail.com>
2022-02-19 22:33:15 +0330

    get & post api with express

--- express.js
+++ express.js
@@ -2,10 +2,19 @@ const express=require('express');
 
 const app=express();
 
+app.use(express.json());
 app.get("/",(req , res)=>{
-res.send("ok")
+res.json({
+    name:"ali",
+    age:"22"
 })
-
+})
+app.post("/",(req,res)=>{
+    console.log(req.body);
+    res.json({
+        status:true
+    })
+})
 app.listen('3000',()=>{
     console.log("conected to 3000")
 })
